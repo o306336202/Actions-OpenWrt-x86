74f079aaefc3f10875b0c9065f0f621c4411f657
o306336202 <o306336202@gmail.com>
2021-07-10 11:31:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
